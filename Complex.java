f42e84652187d5f120840d3990bfcbbc7cb810f9
asapienperson <andrewluo2021@gmail.com>
2025-12-07 11:01:26 -0600

    comments

--- Complex.java
+++ Complex.java
@@ -9,20 +9,32 @@ public class Complex
     //attributes
     private double re;
     private double im;
+    /**
+     * Constructor for complex numbers, given parameters for real and imaginary components
+     */
     public Complex (double real, double imag)
     {
         re = real;
         im = imag;
     }
+    /**
+     * Constructor for complex numbers, gives value of 0
+     */
     public Complex()
     {
         re = 0;
         im = 0;
     }
+    /**
+     * Returns absolute value (magnitude) of complex number
+     */
     public double abs()
     {
         return Math.hypot(re, im);
     }
+    /**
+     * Returns argument (angle) of complex number
+     */
     public double arg()
     {
         if (re == 0)
@@ -40,24 +52,46 @@ public class Complex
         else
         return (Math.PI+Math.atan(im/re));
     }
+    /**
+     * Returns conjugate of complex number
+     */
     public Complex conjugate()
     {
         return new Complex(re,-1*im);
     }
+    /**
+     * Adds complex number z to complex number, returning new complex number
+     */
     public Complex add(Complex z)
     {
         return new Complex(re+z.re,im+z.im);
     }
+    /**
+     * Subtracts complex number z from complex number
+     */
     public Complex subtract(Complex z)
     {
         return new Complex(re-z.re,im-z.im);
     }
+    /**
+     * Multiplies complex number by complex number z
+     */
     public Complex multiply(Complex z)
     {
         return new Complex(re*z.re-im*z.im,re*z.im+z.re*im);
     }
+    /**
+     * Divides complex number by complex number z
+     */
     public Complex divide(Complex z)
     {
         return new Complex((re*z.re+im*z.im)/(z.re*z.re+z.im*z.im),(z.re*im-re*z.im)/(z.re*z.re+z.im*z.im));
+    }
+    /**
+     * Processes complex number as String
+     */
+    public String toString(Complex z)
+    {
+        return (re + "+" + im + "i");
     }
 }
